365133ba75103dba7d18bec032bb7e834e08f5c3
Ahmed <Ahmeddyacoubb@gmail.com>
2023-08-27 18:53:43 +0200

    added style and constants

---
+++ lib/style/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorCustom {
+  static  Color backgroundColor =  Colors.white;
+  static const Color primaryColor = Color.fromARGB(255, 62,99,244);
+  static  Color textBlack = Colors.black;
+  static  Color textWhite = Colors.white;
+}
